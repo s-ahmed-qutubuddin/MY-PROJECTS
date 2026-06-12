66cd8689af01dd9f584ee88fa57c93d5191b368b
s-ahmed-qutubuddin <s.ahmedqutubuddin@gmail.com>
2026-06-12 10:07:06 +0000

    Added choice-story

---
+++ SQL/practice.sql
@@ -0,0 +1,23 @@
+-- Create a table
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(50),
+    age INTEGER NOT NULL,
+);
+INSERT INTO students VALUES
+(1, "Ahmed", 17),
+(2, "Taha",31),
+(3, "Sheema",32);
+SELECT * FROM students;
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(50),
+    age INTEGER NOT NULL,
+);
+INSERT INTO students VALUES
+(1, "Ahmed", 17),
+(2, "Taha",31),
+(3, "Sheema",32);
+SELECT * 
+FROM students;
+
